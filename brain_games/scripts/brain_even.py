361686eb299325c9f3ffe10c54f96ef2e93f6bc9
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-02-07 17:37:25 +0500

    Revert "Delete brain_games/scripts/brain_even.py"
    This reverts commit 10c9aba6ab2cdf7e1a85a918828f834bb0b77817.

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.10
+from random import randint
+from prompt import string
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = string('May I have your name? ')
+    print(f'Hello, {name}\nAnswer "yes" if the number is even, otherwise answer "no".')
+
+    counter = 0
+
+    while counter != 3:
+        randoms_value = randint(0, 1000)
+
+        print(f'Question: {randoms_value}')
+        user_answer = string('Your answer: ')
+
+        if (randoms_value % 2 == 0 and user_answer == 'yes' or
+                randoms_value % 2 != 0 and user_answer == 'no'):
+            counter += 1
+            print('Correct!')
+
+        else:
+            mirror_answer = 'yes' if randoms_value % 2 == 0 else 'no'
+
+            print(
+                f"{user_answer} is wrong answer ;(. Correct answer was {mirror_answer}. \nLet's try again, {name}")
+            break
+    if counter == 3:
+        print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
