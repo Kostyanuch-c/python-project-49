3454526ee879152aa76c960aa432b57aae24fdc0
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-02-07 20:41:52 +0500

    add new game

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,8 +1,34 @@
 from random import randint, choice
+import operator
 import enginy
 
 
-def main():
+def calc_game():
     enginy.welcome_us()
     print('What is the result of the expression?')
 
+    action = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+    }
+    action_key = list(action.keys())
+    counter = 0
+
+    while counter != 3:
+        random_operation = choice(action_key)
+        first_value = randint(10, 200)
+        second_value = randint(1, 100)
+
+        enginy.question(f'{first_value} {random_operation} {second_value}')
+        answer = enginy.user_answer()
+
+        correct_answer = action[random_operation](first_value, second_value)
+        if answer == correct_answer and answer is int:
+            counter += 1
+            print('Correct!')
+        else:
+            enginy.goodbye_user(answer, correct_answer)
+            break
+
+    enginy.congratulations(counter)
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.10
+import games.calc
+
+
+def main():
+    games.calc.calc_game()
+
+
+if __name__ == '__main__':
+    main()
