42556aeb2cb158fa0d1098e963841615b7396b45
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-02-10 15:26:50 +0500

    check mentability

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3.10
-import brain_games.games.gcd
+from brain_games.games.gcd import (welcome_question_gcd,
+                                   problem_and_correct_answer_gcd)
 import brain_games.enginy
 
 
 def main():
-    brain_games.enginy.comparison(
-        brain_games.games.gcd.welcome_question(),
-        brain_games.games.gcd.problem_and_correct_answer)
+    brain_games.enginy.comparison(welcome_question_gcd(),
+                                  problem_and_correct_answer_gcd())
 
 
 if __name__ == '__main__':
