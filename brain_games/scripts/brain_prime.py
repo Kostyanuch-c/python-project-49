fe39a6db9c29a2f72cfbbc051b3f74a7b1bf0753
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-02-10 14:48:53 +0500

    fix sripts

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -3,10 +3,10 @@ import brain_games.games.prime
 import brain_games.enginy
 
 
-def brain_prime():
+def main():
     brain_games.enginy.comparison(brain_games.games.prime.welcome_question(),
                                   brain_games.games.prime.problem_and_correct_answer)
 
 
 if __name__ == '__main__':
-    brain_prime()
+    main()
