e0f37ca9f1cacf6ac45265b8bea222f0981242f5
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-02-10 15:24:29 +0500

    check mentability

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -5,9 +5,8 @@ import brain_games.enginy
 
 
 def main():
-    brain_games.enginy.comparison(
-        welcome_question_prime(),
-        problem_and_correct_answer_prime)
+    brain_games.enginy.comparison(welcome_question_prime(),
+                                  problem_and_correct_answer_prime)
 
 
 if __name__ == '__main__':
