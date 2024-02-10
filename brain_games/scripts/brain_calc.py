a9699bf332ffe09f2faa834bc88b3e86fd5b4252
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-02-10 15:32:21 +0500

    check mentability

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3.10
 from brain_games.games.calc import (welcome_question_calc,
                                     problem_and_correct_answer_calc)
-import brain_games.enginy
+from brain_games.enginy import comparison
 
 
 def main():
-    brain_games.enginy.comparison(
-        welcome_question_calc(),
-        problem_and_correct_answer_calc)
+    comparison(welcome_question_calc(), problem_and_correct_answer_calc)
 
 
 if __name__ == '__main__':
